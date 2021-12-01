e25160e80842e27676574c785219b4c27b0d8510
salamhis2019 <salamehh@msu.edu>
2021-12-01 14:25:51 -0500

    user interface changes completed

--- app.js
+++ app.js
@@ -7,3 +7,29 @@ function readMore() {
     readMoreText.style.display = 'block';
     button = button.remove();
 }
+
+function seeClassification() {
+    // DEFINE UI VARIABLE
+    const imgcontainer = document.getElementById('images-container');
+    const image = document.querySelector('.example-tweet');
+    const button = document.getElementById('btn');
+    const container = document.getElementById('container');
+    const goback = document.querySelector('.go-back')
+
+    // REMOVE OLD IMAGE
+    image.remove();
+
+    // APPEND NEW IMAGE
+    const classified = document.createElement('div');
+    classified.innerHTML = "<img src=\"images/classified-tweet.png\" alt=\"\" class=\"classified-tweet\">"
+    imgcontainer.appendChild(classified);
+
+    // BUTTON EDITS
+    button.remove();
+
+    // ADD NEW BUTTON
+    const cont = document.createElement('div');
+    cont.classList.add('btn-spacing')
+    cont.innerHTML = '<a id="btn" href="#">Continue</a>';
+    container.insertBefore(cont, goback)
+}
